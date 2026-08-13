2566eac5dcb168dcde9989bb29886e8702d6f7f6
el mehdi soukli <mahdisoukli1@gmail.com>
2026-08-13 22:51:05 +0100

    Amélioration du menu responsive

--- script.js
+++ script.js
@@ -1,28 +1,53 @@
 let menu=document.querySelector(".menu");
 let menu_list=document.querySelector(".menu_list");
+let block=document.querySelector(".block")
+let footer=document.querySelector("footer")
+let at=document.querySelector(".attribution")
 
-menu.addEventListener("click",function(){
-    menu.innerHTML=menu.innerHTML==='<h1 class="menu_title">=</h1>'?` <ul class="menu_list">
-      <li><a href="#">About</a></li>
-      <li><a href="#">Our Work</a></li>
-      <li><a href="#">Partners</a></li>
-      <li><a href="#">Annual Report</a></li>
-      <li><a href="#">Donate</a></li>
-    </ul>`:`<h1 class="menu_title">=</h1>`;
-})
+menu.addEventListener("click", function() {
+  if (menu.innerHTML.includes("icon-menu.svg")) {
+    // Affiche le menu avec bouton X
+    menu.innerHTML = `
+      <div class="menu">
+        <div class="X">X</div>
+        <ul class="menu_list">
+          <li><a href="#">About</a></li>
+          <li><a href="#">Our Work</a></li>
+          <li><a href="#">Partners</a></li>
+          <li><a href="#">Annual Report</a></li>
+          <li><a href="#">Donate</a></li>
+        </ul>
+      </div>
+    `;
+    block.classList.toggle("ops"); // toggle séparé
+    footer.classList.toggle("ops")
+    at.classList.toggle("ops")
+  } else {
+    // Affiche l’icône menu
+    menu.innerHTML = `<img class="menu_title" src="./assets/images/icon-menu.svg" alt="">`;
+    block.classList.remove("ops"); // ferme le menu
+    footer.classList.remove("ops")
+    at.classList.remove("ops")
+  }
+});
+
 
 window.addEventListener("resize",function(){
 if(window.innerWidth<768){ 
-  menu.innerHTML=menu.innerHTML=`<h1 class="menu_title">=</h1>`
+  menu.innerHTML=menu.innerHTML=`<img class="menu_title" src="./assets/images/icon-menu.svg" alt="">`
   
 } 
 else{
-  menu.innerHTML=menu.innerHTML=`<ul class="menu_list">
+  menu.innerHTML=menu.innerHTML=`
+  <div class="menu">
+     <div class="X">X</div>
+  <ul class="menu_list">
       <li><a href="#">About</a></li>
       <li><a href="#">Our Work</a></li>
       <li><a href="#">Partners</a></li>
       <li><a href="#">Annual Report</a></li>
       <li><a href="#">Donate</a></li>
-    </ul>`
+    </ul>
+    </div>`
 }
 })
